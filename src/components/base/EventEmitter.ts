d707fc8ad6dd2918d54106ed2d56bc0ef29b90c9
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-01-18 23:13:38 +0700

    feat(add base class)

--- src/components/base/EventEmitter.ts
+++ src/components/base/EventEmitter.ts
@@ -1,17 +1,9 @@
-// Хорошая практика даже простые типы выносить в алиасы
-// Зато когда захотите поменять это достаточно сделать в одном месте
-type EventName = string | RegExp;
-type Subscriber = Function;
-type EmitterEvent = {
-    eventName: string,
-    data: unknown
-};
-
-export interface IEvents {
-    on<T extends object>(event: EventName, callback: (data: T) => void): void;
-    emit<T extends object>(event: string, data?: T): void;
-    trigger<T extends object>(event: string, context?: Partial<T>): (data: T) => void;
-}
+import { 
+    IEvents, 
+    EventName, 
+    Subscriber, 
+    EmitterEvent 
+} from '../../types/components/base/EventEmitter';
 
 /**
  * Брокер событий, классическая реализация
@@ -88,4 +80,3 @@ export class EventEmitter implements IEvents {
         };
     }
 }
-
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,10 @@
-// import { Setting } from '../types/settings';
+import { Settings } from '../types/settings';
 
 export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
 export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
 export const DEVELOPMENT = process.env.NODE_ENV === 'development';
 
 // Настройка приложения
-export const SETTINGS = {
+export const SETTINGS: Settings = {
     // ...Буду дополнять в реализации...
 };
